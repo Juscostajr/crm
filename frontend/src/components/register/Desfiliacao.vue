7df261b54de2a4566d7da722254c1618bbd9fe13
Juscostajr <juscostajr@gmail.com>
2018-11-15 13:34:23 -0200

    Desfiliação complemento

---
+++ frontend/src/components/register/Desfiliacao.vue
@@ -0,0 +1,101 @@
+<template>
+<el-dialog :title="title" :visible.sync="dialogVisible" width="30%">
+    <el-form :model="form">
+      <el-form-item label="Associado">
+          <el-input :value="associado.pessoaJuridica.nomeFantasia" readonly></el-input>
+      </el-form-item>
+      <el-form-item label="Data">
+          <el-date-picker v-model="form.data" format="dd/MM/yyyy"></el-date-picker>
+      </el-form-item>
+      <el-form-item label="Motivo">
+          <el-select v-model="form.motivo">
+            <el-option v-for="(motivo,index) in motivoDesfiliacao" :key="index" :value="motivo"></el-option>
+          </el-select>
+      </el-form-item>
+    </el-form>
+     <div slot="footer">
+      <el-button @click="dialogVisible = false">Cancelar</el-button>
+      <el-button type="success" @click="save">Salvar</el-button>
+      {{this.usuario}}
+    </div>
+</el-dialog>    
+</template>
+<script>
+export default {
+  props: {
+    visible: Boolean,
+    associado: Object,
+  },
+  computed:{
+      usuario(){
+       // alert(this.$store.state.usuario.login);
+      }
+  },
+  data() {
+    return {
+      title: "Desfiliar Associado",
+      dialogVisible: false,
+      form: {
+        data: new Date(),
+        motivo: '',
+      },
+      motivoDesfiliacao: [],
+      errorMessage: {
+        404: "Rota não encontrada",
+        500: "Ocorreu um erro inesperado no servidor"
+      }
+    };
+  },
+  watch: {
+    visible() {
+      this.dialogVisible = this.visible;
+    },
+    dialogVisible() {
+      if (!this.dialogVisible) {
+        this.$emit("update:visible", this.dialogVisible);
+      }
+    }
+  },
+  methods: {
+    save() {
+      this.form.associado = this.associado;
+      this.$request
+        .post("desfiliacao", this.form)
+        .then(response => {
+          this.$notify({
+            title: "Sucesso!",
+            message: "Desfiliação registrada!",
+            type: "success"
+          });
+        })
+        .catch(error => {
+          let message =
+            error.response.hasOwnProperty("status") &&
+            this.errorMessage.hasOwnProperty(error.response.status)
+              ? this.errorMessage[error.response.status]
+              : "Houve um erro inesperado";
+
+          this.$notify.error({
+            title: "Erro!",
+            message: message
+          });
+          console.error(error);
+        })
+        .finally(() => {
+          this.dialogVisible = false;
+        });
+    }
+  },
+  mounted() {
+      this.$request.get('desfiliacao/motivo')
+      .then(response => {
+          this.motivoDesfiliacao = response.data;
+      })
+
+  },
+};
+</script>
+<style scoped>
+</style>
+
+
--- frontend/src/pages/Posvenda.vue
+++ frontend/src/pages/Posvenda.vue
@@ -1,15 +1,52 @@
-s<template>
-    <h2>{{msg}}</h2>
+<template>
+<div>
+<el-card header="Associados">
+    <el-form :model="form">
+        <el-table :data="associados">
+            <el-table-column type="index" width="50"/>
+            <el-table-column label="Nome" property="pessoaJuridica.nomeFantasia" />
+            <el-table-column label="Data de Filiação">
+                <template slot-scope="scope">
+                {{scope.row.dataFiliacao | moment('DD/MM/YYYY')}}
+                </template>
+            </el-table-column>
+            <el-table-column label="Valor da Mensalidade" property="valorMensalidade"/>
+            <el-table-column fixed="right" label="Ação" width="120">
+                <template slot-scope="scope">
+                    <el-button size="mini" type="warning" @click="handleDesfiliar(scope.row)"><icon name="lock"/> Desfiliar</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+    </el-form>
+    <desfiliacao :visible.sync="desfiliacaoVisible" :associado="currentAssociado"/>
+</el-card>
+</div>
 </template>
 <script>
-    export default {
-        data () {
-            return {
-                msg: 'Posvenda'
-            }
-        },
-        components: {},
-        methods: {},
-    }
+import Desfiliacao from '../components/register/Desfiliacao.vue';
+export default {
+  data() {
+    return {
+      msg: "Posvenda",
+      associados: [],
+      form: {},
+      desfiliacaoVisible: false,
+      currentAssociado: {}
+    };
+  },
+  components: { Desfiliacao },
+  methods: {
+      handleDesfiliar(data){
+          this.currentAssociado = data;
+          this.desfiliacaoVisible = true;
+      }
+  },
+  mounted() {
+    this.$request.get("associado/status/Ativo").then(response => {
+      this.associados = response.data;
+      console.log(this.associados);
+    });
+  }
+};
 </script>
 <style></style>
